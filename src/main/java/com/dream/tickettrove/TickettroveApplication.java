d7025846bb69c622286b71b51bbe68387a09a2ac
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-09-01 21:45:08 +0800

    chore: initialize backend and api

---
+++ src/main/java/com/dream/tickettrove/TickettroveApplication.java
@@ -0,0 +1,13 @@
+package com.dream.tickettrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickettroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickettroveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/tickettrove/TickettroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.tickettrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickettroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
