96c905668b9172f0b65a9e6b5ae20febdd909cfc
Daphne Balane <balanedaphne20@gmail.com>
2023-09-07 21:36:46 +0800

    [ST004] feat: added Cinema in movie service

--- src/main/java/com/dream/tickettrove/service/MovieService.java
+++ src/main/java/com/dream/tickettrove/service/MovieService.java
@@ -1,12 +1,16 @@
 package com.dream.tickettrove.service;
 
 import com.dream.tickettrove.exception.MovieNotFoundException;
+import com.dream.tickettrove.model.Cinema;
 import com.dream.tickettrove.model.Movie;
 import com.dream.tickettrove.model.Showtime;
+import com.dream.tickettrove.repository.CinemaLocationRepository;
 import com.dream.tickettrove.repository.MovieRepository;
 import com.dream.tickettrove.repository.ShowtimeRepository;
 import com.dream.tickettrove.service.dto.MovieResponse;
+import com.dream.tickettrove.service.dto.ShowtimeResponse;
 import com.dream.tickettrove.service.mapper.MovieMapper;
+import com.dream.tickettrove.service.mapper.ShowtimeMapper;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -20,10 +24,12 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
     private final ShowtimeRepository showtimeRepository;
+    private final CinemaLocationRepository cinemaLocationRepository;
 
-    public MovieService(MovieRepository movieRepository, ShowtimeRepository showtimeRepository) {
+    public MovieService(MovieRepository movieRepository, ShowtimeRepository showtimeRepository, CinemaLocationRepository cinemaLocationRepository) {
         this.movieRepository = movieRepository;
         this.showtimeRepository = showtimeRepository;
+        this.cinemaLocationRepository = cinemaLocationRepository;
     }
 
     public List<MovieResponse> findAllNowShowing() {
@@ -48,8 +54,14 @@ public class MovieService {
         return showtime.getStartTime().after(currentDate) && showtime.getEndTime().after(currentDate);
     }
 
-    public List<Showtime> findDate(Integer id) {
-        return showtimeRepository.findByMovieId(id);
+    public List<Cinema> findCinemasByMovieAndDate(Integer id, String date) {
+        return showtimeRepository.findByMovieId(id)
+                .stream()
+                .filter(showtime -> isShowtimeChosenDate(showtime.getStartTime(), date))
+                .sorted(Comparator.comparing(Showtime::getStartTime))
+                .map(showtime -> getCinema(showtime.getCinemaId()))
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public List<Showtime> getShowtimesByMovieCinemaDate(Integer id, Long cinemaId, String date) {
@@ -65,4 +77,10 @@ public class MovieService {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         return dateFormat.format(startTime).equals(date);
     }
+
+    private Cinema getCinema(Long cinemaId) {
+        return cinemaLocationRepository.findById(cinemaId)
+                .orElseThrow(MovieNotFoundException::new);
+    }
+
 }
