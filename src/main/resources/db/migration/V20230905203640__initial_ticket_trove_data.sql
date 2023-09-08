3cc16f1bfb8821bc55af2242b4b22d0e446d2ab1
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-09-08 04:01:39 +0100

    chore: change initial app data

--- src/main/resources/db/migration/V20230905203640__initial_ticket_trove_data.sql
+++ src/main/resources/db/migration/V20230905203640__initial_ticket_trove_data.sql
@@ -2,7 +2,7 @@ SET time_zone = '+08:00';
 
 INSERT INTO `movie` (`title`, `poster`, `synopsis`, `short_description`, `release_date`, `runtime`, `rating`, `director`, `actors`)
 VALUES
-  ('The Nun II', 'https://cdn1.clickthecity.com/images/movies/poster/400/19136_3.jpg', 'In 1956 France, a priest is murdered, and it seems an evil is spreading. Sister Irene once again comes face to face with a demonic force.', 'Sister Irene returns once again and comes face to face with the demonic force Valak, the Nun.', '2023-09-08', 109, 'Rated R', 'Michael Chaves', 'Taissa Farmiga, Jonas Bloquet, Storm Reid'),
+  ('The Nun II', 'https://cdn1.clickthecity.com/images/movies/poster/400/19136_3.jpg', 'In 1956 France, a priest is murdered, and it seems an evil is spreading. Sister Irene once again comes face to face with a demonic force.', 'Sister Irene returns once again and comes face to face with the demonic force Valak.', '2023-09-08', 109, 'Rated R', 'Michael Chaves', 'Taissa Farmiga, Jonas Bloquet, Storm Reid'),
   ('Deliver Us', 'https://cdn1.clickthecity.com/images/movies/poster/400/19481_2.jpg', 'A nun pregnant with twins finds herself the target of a terrifying religious sect convinced she’s about to give birth to the apocalypse. As she races into hiding, her only ally is a young priest undergoing a crisis of faith.', 'A nun pregnant with twins finds herself the target of a terrifying religious sect convinced she is about to give birth to the apocalypse', '2023-09-09', 90, 'Rated R', 'Cru Ennis, Lee Roy Kunz', 'Jaune Kimmel, Thomas Kretschmann, Lee Roy Kunz'),
   ('Teenage Mutant Ninja Turtles: Mutant Mayhem', 'https://cdn1.clickthecity.com/images/movies/poster/400/19167_4.jpg', 'After years of being sheltered from the human world, the Turtle brothers set out to win the hearts of New Yorkers and be accepted as normal teenagers through heroic acts.', 'Turtle brothers set out to win the hearts of New Yorkers', '2023-09-02', 100, 'PG', 'Jeff Rowe', 'Nicolas Cantu, Shamon Brown Jr, Micah Abbey'),
   ('Retribution', 'https://cdn1.clickthecity.com/images/movies/poster/400/19481_2.jpg', 'Matt Turner is a successful Berlin based American businessman juggling a booming financial career with family responsibility', 'Driving his kids to school one morning, Matt receives a phone call from a mysterious voice: there’s a bomb under his seat that will detonate unless he completes a specific series of tasks, and fast', '2023-08-23', 100, 'PG', 'Nimrod Antal', 'Liam Neeson, Jack Champion, Emily Kusche'),
@@ -55,13 +55,13 @@ INSERT INTO `seat` (`hall_id`, `grid_row`, `grid_col`, `is_available`) VALUES
 
 INSERT INTO `showtime` (`movie_id`, `cinema_id`, `hall_id`, `start_time`, `end_time`)
 VALUES
-    (1, 1, 1, '2023-09-08 10:00:00', '2023-09-08 12:00:00'),
-    (1, 1, 2, '2023-09-08 14:00:00', '2023-09-08 16:00:00'),
-    (1, 2, 3, '2023-09-08 10:00:00', '2023-09-08 12:00:00'),
-    (1, 3, 5, '2023-09-08 14:00:00', '2023-09-08 16:00:00'),
-    (1, 3, 5, '2023-09-09 14:00:00', '2023-09-09 16:30:00'),
-    (1, 3, 5, '2023-09-09 16:00:00', '2023-09-09 18:30:00'),
-    (1, 2, 3, '2023-09-10 10:00:00', '2023-09-10 12:30:00');
+    (1, 1, 1, '2023-09-08 17:00:00', '2023-09-08 19:00:00'),
+    (1, 1, 2, '2023-09-08 19:00:00', '2023-09-08 21:00:00'),
+    (1, 2, 3, '2023-09-08 18:00:00', '2023-09-08 20:00:00'),
+    (1, 3, 5, '2023-09-08 17:00:00', '2023-09-08 18:00:00'),
+    (1, 3, 5, '2023-09-09 17:00:00', '2023-09-09 18:30:00'),
+    (1, 3, 5, '2023-09-09 18:00:00', '2023-09-09 20:30:00'),
+    (1, 2, 3, '2023-09-10 19:00:00', '2023-09-10 21:30:00');
 
 
 INSERT INTO `showtime` (`movie_id`, `cinema_id`, `hall_id`, `start_time`, `end_time`)
@@ -116,6 +116,83 @@ INSERT INTO `showtime` (`movie_id`, `cinema_id`, `hall_id`, `start_time`, `end_t
 VALUES
     (13, 3, 5, '2023-09-11 16:00:00', '2023-09-11 18:00:00'),
     (13, 3, 5, '2023-09-11 18:00:00', '2023-09-11 20:00:00');
+
+
 Update movie
-set  short_description   = 'Action-packed Thriller'
+set  short_description = 'Mission: Impossible is based on the TV series by Bruce Geller.',
+title ='Mission: Impossible '
 where id = 13;
+Update movie
+set
+title ='IU Concert'
+where id = 11;
+Update movie
+set
+short_description ='She decides to go to the Real World '
+where id = 6;
+Update movie
+set
+short_description ='A nun pregnant with twins the target of a terrifying religious sect'
+where id = 2;
+Update movie
+set
+short_description ='Sister Irene returns to face  the demonic force Valak.'
+where id = 1;
+
+
+
+UPDATE `movie`
+SET `short_description` =
+  'Sister Irene returns once again and comes face to face with the demonic force Valak.'
+WHERE `title` = 'The Nun II';
+
+UPDATE `movie`
+SET `short_description` =
+  'Pregnant nun targeted by terrifying religious sect, aided by a young priest in a faith crisis.'
+WHERE `title` = 'Deliver Us';
+
+UPDATE `movie`
+SET `short_description` =
+  'Turtle brothers seek acceptance in NYC through heroic acts after sheltered life years.',
+  title ='TMNT: Mutant Mayhem'
+where id = 3;
+
+UPDATE `movie`
+SET `short_description` =
+  'Berlin businessman Matt Turner races to complete tasks to defuse a bomb under his car seat.'
+WHERE `title` = 'Retribution';
+
+UPDATE `movie`
+SET `short_description` =
+  'The team faces chaos as a malevolent mining operation threatens their deep-sea mission.'
+WHERE `title` = 'Meg 2 The Trench';
+
+UPDATE `movie`
+SET `short_description` =
+  'Barbie societal impact explored, embarks on a quest to find a cure in the Real World.'
+WHERE `title` = 'BARBIE';
+
+UPDATE `movie`
+SET `short_description` =
+  'Kindergartener Shinnosuke gains telekinetic superpowers after a cosmic event on Earth.'
+WHERE `title` = 'Crayon Shin-Chan';
+
+UPDATE `movie`
+SET `short_description` =
+  'Reggie, abandoned in the city, believes his owner would never leave him on purpose.'
+WHERE `title` = 'Strays';
+
+UPDATE `movie`
+SET `short_description` =
+  'Hercule Poirot attends a Halloween séance in a haunted palazzo, reluctantly, in Venice.'
+WHERE `title` = 'A Haunting in Venice';
+
+UPDATE `movie`
+SET `short_description` =
+  'Robert McCall discovers his friends are under the control of local crime bosses in Italy.'
+WHERE `title` = 'Equalizer 3';
+
+UPDATE `movie`
+SET `short_description` =
+  'IU CONCERT: The Golden Hour celebrates the 15th anniversary of IU debut in a Cinematic Cut.'
+WHERE `title` = 'IU Concert: The Golden Hour';
